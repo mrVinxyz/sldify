145c476bc622d8265ff779ef4b8184783b8b4dea
mrvin <vinicius@mrvin.xyz>
2024-08-07 21:55:27 -0400

    Enabled linter and formatter for the doc entry point `index.tsx`

--- src/doc/index.tsx
+++ src/doc/index.tsx
@@ -1,16 +1,16 @@
-import "./index.css";
+import './index.css';
 
-import { render } from "solid-js/web";
-import {Route, Router, RouteSectionProps} from "@solidjs/router";
-import {JSX} from "solid-js";
-import DropdownHome from "./dropdown/dropdown-home";
+import { render } from 'solid-js/web';
+import { Route, Router, type RouteSectionProps } from '@solidjs/router';
+import type { JSX } from 'solid-js';
+import DropdownHome from './dropdown/dropdown-home';
 
 render(() => <Routing />, document.body);
 
 function Routing() {
 	return (
 		<Router root={Layout}>
-			<Route path="/dropdown" component={DropdownHome} />
+			<Route path='/dropdown' component={DropdownHome} />
 		</Router>
 	);
 }
@@ -18,5 +18,5 @@ function Routing() {
 function Layout(props: RouteSectionProps): JSX.Element {
 	return (
 		<div class={'p-4 bg-gray-50 w-screen h-screen'}>{props.children}</div>
-	)
+	);
 }
