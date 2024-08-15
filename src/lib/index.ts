8d1e8829d765c2cfd3dc183c10f70c070e07d3ba
mrvin <vinicius@mrvin.xyz>
2024-08-14 22:35:12 -0400

    Modal draft

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,5 +2,6 @@ import { Dropdown } from './dropdown/dropdown';
 import { Input } from './input/input';
 import { Form } from './form/form';
 import { Field, useField } from './form/field';
+import { Modal } from './modal/modal';
 
-export { Dropdown, Input, Form, Field, useField };
+export { Dropdown, Input, Form, Field, useField, Modal };
