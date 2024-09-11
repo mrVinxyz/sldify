3492bd216c8ecd4ad48ab612a6c27bbb3846997a
mrvin <vinicius@mrvin.xyz>
2024-09-11 07:36:44 -0400

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
             formats: ['es']
         },
         rollupOptions: {
-            external: ['solid-js'],
+            external: ['solid-js', "class-variance-authority"],
         },
         target: "esnext"
     },
