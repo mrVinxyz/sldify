c2ed8e81ae56bd058d779598668c097566ad3fc1
mrvin <vinicius@mrvin.xyz>
2024-09-11 07:40:09 -0400

    Add optional class prop to TextArea component

--- src/lib/input/text-area.tsx
+++ src/lib/input/text-area.tsx
@@ -12,6 +12,8 @@ type TextAreaProps = {
 	rows?: number;
 	/** Whether the textarea field is disabled. */
 	disabled?: boolean;
+	/** Optional additional class names for the input field. */
+	class?: string;
 	/** Additional properties for the textarea element. */
 	[key: string]: unknown;
 };
@@ -23,15 +25,16 @@ type TextAreaProps = {
  * @returns {JSX.Element} - The rendered TextArea component.
  */
 export const TextArea = (props: TextAreaProps): JSX.Element => {
-	const [prop, others] = splitProps(props, ['name', 'placeholder', 'disabled', 'rows']);
+	const [prop, others] = splitProps(props, ['name', 'placeholder', 'disabled', 'rows', 'class']);
 
 	return (
 		<textarea
 			id={prop.name + 'Input'}
 			name={prop.name}
 			classList={{
-				'block w-full rounded-md text-sm p-2.5 bg-gray-50 border border-gray-200 text-gray-800 font-medium focus:ring-blue-600 focus:border-blue-600': true,
+				'block w-full rounded-sm text-sm p-2.5 bg-gray-50 border border-gray-200 text-gray-800 font-medium focus:ring-blue-600 focus:border-blue-600 focus:outline-none': true,
 				'cursor-not-allowed': prop.disabled,
+				[prop.class || '']: !!prop.class
 			}}
 			disabled={prop.disabled}
 			placeholder={props.placeholder}
