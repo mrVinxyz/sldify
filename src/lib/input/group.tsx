7d5c4fe8d974055f0f2d886788342532bcad9a39
mrvin <vinicius@mrvin.xyz>
2024-09-11 07:46:38 -0400

    Refactor InputGroup size handling to use cva

--- src/lib/input/group.tsx
+++ src/lib/input/group.tsx
@@ -1,18 +1,18 @@
 import type { Element } from '../types';
+import { cva } from 'class-variance-authority';
 
-/**
- * Props for the Group component.
- */
-export type GroupProps = {
+/** Props for the Group component. */
+export type InputGroupProps = {
 	/** The child elements to be rendered inside the group. */
 	children: Element;
-	/** The input field size. Default `md`*/
+	/** The input field size. */
 	size?: InputGroupSize;
 	/** The id of the group element. */
 	id?: string;
 };
 
-/** The input field size.
+/**
+ * The input field size.
  * @values xs=2; sm=4; md=6; lg=8; xl=10; full=12;
  */
 export type InputGroupSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'full';
@@ -20,21 +20,30 @@ export type InputGroupSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'full';
 /**
  * Group component to wrap child elements with additional styling.
  *
- * @param {GroupProps} props - The properties for the Group component.
+ * @param {InputGroupProps} props - The properties for the Group component.
  * @returns {JSX.Element} - The rendered Group component.
  */
-export const InputGroup = (props: GroupProps): Element => {
+export const InputGroup = (props: InputGroupProps): Element => {
+	const groupClasses = cva('relative w-full col-span-12', {
+		variants: {
+			size: {
+				xs: 'md:col-span-2',
+				sm: 'md:col-span-4',
+				md: 'md:col-span-6',
+				lg: 'md:col-span-8',
+				xl: 'md:col-span-10',
+				full: 'md:col-span-12',
+			},
+		},
+		defaultVariants: {
+			size: 'full',
+		},
+	});
+
 	return (
 		<div
-			classList={{
-				'relative w-full col-span-12': true,
-				'md:col-span-2': props.size === 'xs',
-				'md:col-span-4': props.size === 'sm',
-				'sm:col-span-6': props.size === 'md',
-				'md:col-span-8': props.size === 'lg',
-				'md:col-span-10': props.size === 'xl',
-				'md:col-span-12': props.size === 'full',
-			}}
+			id={props.id}
+			class={groupClasses({ size: props.size })}
 		>
 			{props.children}
 		</div>
