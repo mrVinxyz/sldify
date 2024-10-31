c87898182c8c1da38e00123ce7a67786e851f259
mrvin <vinicius@mrvin.xyz>
2024-10-30 21:56:09 -0400

    feat: enhance input-select flexibility and robustness

--- pkg/components/input/input-select.tsx
+++ pkg/components/input/input-select.tsx
@@ -1,8 +1,9 @@
-import { For } from 'solid-js';
+import { createSignal, For, splitProps } from 'solid-js';
 import { Input, type InputProps } from './input';
-import { Collapsible } from '../collapsible/collapsible';
+import { Collapsible, createCollapsible } from '../collapsible/collapsible';
 import { CollapsibleControl } from '../collapsible/control';
 import { CollapsibleContent } from '../collapsible/content';
+import { cva } from 'class-variance-authority';
 
 type InputSelectOption<T> = {
 	label: string;
@@ -13,29 +14,81 @@ type InputSelectProps<T> = InputProps & {
 	options: InputSelectOption<T>[];
 	value?: T;
 	onSelected?: (option: InputSelectOption<T>) => void;
+	onOpen?: () => void;
+	containerSize?: 'sm' | 'md' | 'lg' | 'xl';
+	containerClass?: string;
+	optionItemSize?: 'sm' | 'md' | 'lg';
+	optionItemClass?: string;
 };
 
-function InputSelect<T>(props: InputSelectProps<T>) {
-	const Option = (optionProp: InputSelectOption<T>) => (
-		<a
-			href='#'
-			class='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'
-			onClick={(e) => {
-				e.preventDefault();
-				props.onSelected?.(optionProp);
-			}}
-		>
-			{optionProp.label}
-		</a>
-	);
+const optionsContainerVariants = cva(
+	'z-10 absolute w-full mt-1 bg-white border divide-y divide-gray-200 rounded-lg dark:bg-gray-700 dark:divide-gray-600 overflow-y-auto',
+	{
+		variants: {
+			size: {
+				sm: 'h-36',
+				md: 'h-48',
+				lg: 'h-60',
+				xl: 'h-80',
+			},
+		},
+		defaultVariants: {
+			size: 'md',
+		},
+	},
+);
 
-	const OptionsMap = () => (
-		<div class='z-10 absolute w-full mt-1 bg-white divide-y divide-gray-100 rounded-lg dark:bg-gray-700'>
+const optionItemVariants = cva(
+	'font-medium block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white',
+	{
+		variants: {
+			size: {
+				sm: 'px-2 py-1',
+				md: 'px-4 py-2',
+				lg: 'px-6 py-3',
+			},
+		},
+		defaultVariants: {
+			size: 'md',
+		},
+	},
+);
+
+function InputSelect<T>(props: InputSelectProps<T>) {
+	const [local, rest] = splitProps(props, ['options', 'value', 'onSelected']);
+	const [value, setValue] = createSignal<InputSelectOption<T>>();
+
+	const Option = (optionProp: InputSelectOption<T>) => {
+		return (
+			<a
+				href='#'
+				class={optionItemVariants({
+					size: props.optionItemSize,
+					class: props.optionItemClass,
+				})}
+				onClick={() => {
+					local.onSelected?.(optionProp);
+					collapsible.hide();
+					setValue(optionProp);
+				}}
+			>
+				{optionProp.label}
+			</a>
+		);
+	};
+
+	const OptionsContainer = () => (
+		<div
+			class={optionsContainerVariants({
+				size: props.containerSize,
+				class: props.containerClass,
+			})}
+		>
 			<ul
 				class='py-2 text-sm text-gray-700 dark:text-gray-200'
 				aria-labelledby='dropdownDefaultButton'
 			>
-				<For each={props.options}>
+				<For each={local.options}>
 					{(option) => (
 						<li>
 							<Option {...option} />
@@ -46,25 +99,32 @@ function InputSelect<T>(props: InputSelectProps<T>) {
 		</div>
 	);
 
-	const getCurrentLabel = () => {
-		if (!props.value) return '';
-		const selectedOption = props.options.find((opt) => opt.value === props.value);
-		return selectedOption?.label || '';
+	const ThisInput = (props: { onClick: (e: MouseEvent) => void }) => {
+		const handleClick = (
+			e: MouseEvent & { currentTarget: HTMLInputElement; target: Element },
+		) => {
+			const otherClick = rest.onClick;
+			if (otherClick && typeof otherClick === 'function') otherClick(e);
+			props.onClick(e);
+		};
+
+		return (
+			<Input
+				value={value()?.label as string | number | string[] | undefined}
+				{...rest}
+				onClick={handleClick}
+				data-selectedValue={local.value}
+			/>
+		);
 	};
 
-	const ThisInput = (props: InputProps) => (
-		<Input
-			placeholder={'does this works?'}
-			value={getCurrentLabel()}
-			{...props}
-		/>
-	);
+	const collapsible = createCollapsible();
 
 	return (
-		<Collapsible>
+		<Collapsible ctx={collapsible}>
 			<CollapsibleControl asChild={ThisInput} />
 			<CollapsibleContent>
-				<OptionsMap />
+				<OptionsContainer />
 			</CollapsibleContent>
 		</Collapsible>
 	);
