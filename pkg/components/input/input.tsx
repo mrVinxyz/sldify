0219795416159128a9497b93e874e0f404dfc708
mrvin <vinicius@mrvin.xyz>
2024-11-03 21:23:42 -0400

    feat: add more styling options

--- pkg/components/input/input.tsx
+++ pkg/components/input/input.tsx
@@ -8,6 +8,7 @@ const inputVariants = cva(
 	{
 		variants: {
 			variant: {
+				plain: '',
 				default:
 					'bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:ring-blue-500 dark:focus:ring-blue-500 dark:focus:border-blue-500',
 				success:
@@ -15,6 +16,7 @@ const inputVariants = cva(
 				error: 'bg-red-50 border border-red-500 text-red-900 dark:text-red-400 placeholder-red-700 dark:placeholder-red-500 focus:ring-red-500 focus:border-red-500 dark:bg-gray-700 dark:border-red-500',
 			},
 			size: {
+				xs: 'p-1.5',
 				sm: 'p-2',
 				md: 'p-2.5',
 				lg: 'p-3',
@@ -24,6 +26,8 @@ const inputVariants = cva(
 				sm: 'rounded-sm',
 				md: 'rounded-md',
 				lg: 'rounded-lg',
+				xl: 'rounded-xl',
+				full: 'rounded-full',
 			},
 		},
 		compoundVariants: [
