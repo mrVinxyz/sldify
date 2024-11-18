181f3c86906b90050b508e2cd7187e91506f9f1c
mrvin <vinicius@mrvin.xyz>
2024-11-17 22:18:13 -0400

    refactor: fusing 'portal' and content together for simplified API

--- pkg/components/collapsible/content.tsx
+++ pkg/components/collapsible/content.tsx
@@ -1,26 +1,41 @@
-import type { ChildProp } from '../../utils/types';
-import { type JSX, splitProps } from 'solid-js';
+import {createEffect, createSignal, type JSX, Show, splitProps} from 'solid-js';
 import { useCollapsible } from './collapsible';
 
 type CollapsibleLayout = 'float' | 'fixed';
 
 type CollapsibleContentProps = {
 	layout: CollapsibleLayout;
-} & JSX.IntrinsicElements['div'] &
-	ChildProp;
+	autoMount?: boolean;
+} & JSX.IntrinsicElements['div'];
 
 const CollapsibleContent = (props: CollapsibleContentProps) => {
 	const [local, rest] = splitProps(props, ['layout', 'class']);
+	const [lazyMounted, setLazyMounted] = createSignal(false);
 	const collapsible = useCollapsible();
+
+	createEffect(() => {
+		if (collapsible.isActive() && !lazyMounted()) {
+			setLazyMounted(true);
+		}
+	});
+
 	return (
-		<div
-			id={collapsible.id().concat('-content')}
-			class={`${local.layout === 'float' ? 'absolute' : 'relative'} ${local.class || ''}`}
-			{...rest}
-		>
-			{props.children}
-		</div>
+		<Show when={props.autoMount || lazyMounted()}>
+			<div
+				id={collapsible.id().concat('-portal')}
+				class={`relative z-10 ${!collapsible.isActive() ? 'hidden' : ''}`}
+			>
+				<div
+					id={collapsible.id().concat('-content')}
+					class={`${local.layout === 'float' ? 'absolute' : 'relative'} ${local.class || ''}`}
+					{...rest}
+				>
+					{props.children}
+				</div>
+			</div>
+		</Show>
 	);
 };
+
 
 export { type CollapsibleContentProps, type CollapsibleLayout, CollapsibleContent };
