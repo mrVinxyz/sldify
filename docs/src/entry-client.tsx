1cfb3da4c2058b984b753792ab82e4207748f1b5
mrvin <vinicius@mrvin.xyz>
2024-11-27 22:13:03 -0400

    storing previous docs for later usage

--- docs/src/entry-client.tsx
+++
@@ -1,4 +0,0 @@
-import { mount, StartClient } from '@solidjs/start/client';
-
-// biome-ignore lint/style/noNonNullAssertion: <initial setup>
-mount(() => <StartClient />, document.getElementById('app')!);
