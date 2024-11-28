4d82a21b08d534287cd383bd965aae563857ddb9
mrvin <vinicius@mrvin.xyz>
2024-11-27 22:19:16 -0400

    .

--- docs/src/app.tsx
+++ docs/src/app.tsx
@@ -3,15 +3,10 @@ import { Router } from '@solidjs/router';
 import { FileRoutes } from '@solidjs/start/router';
 import { type JSX, Suspense } from 'solid-js';
 import './app.css';
-import ColorModeToggler from 'pkg/components/button/color-mode-toggler';
 
 export default function App() {
 	const Layout = (props: { children: JSX.Element }) => (
 		<div class={'h-screen w-screen overflow-y-auto p-4 bg-gray-50 dark:bg-neutral-900'}>
-			<ColorModeToggler
-				class={'fixed'}
-				textContent={'toggle'}
-			/>
 			{props.children}
 		</div>
 	);
--- docs/src/routes/index.tsx
+++ docs/src/routes/index.tsx
@@ -1,12 +1,7 @@
-import ColorModeToggler from "pkg/components/button/color-mode-toggler";
-
 export default function Home() {
 	return (
 		<main class={'h-screen w-screen overflow-y-auto p-4 bg-gray-50 dark:bg-neutral-900'}>
-			<ColorModeToggler
-				class={'fixed'}
-				textContent={'toggle'}
-			/>
+
 		</main>
 	);
 }
